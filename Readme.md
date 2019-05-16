d0d3bb42f7ca3a3ccc5f36bd02def367eb33d153
BrianDX <boris.zaitsev@devexpress.com>
2019-05-16 18:54:16 +0300

    update 19.1.3+

--- Readme.md
+++ Readme.md
@@ -4,11 +4,10 @@
 * [MainWindow.xaml](./CS/WpfPivotGridFilterValues/MainWindow.xaml) (VB: [MainWindow.xaml](./VB/WpfPivotGridFilterValues/MainWindow.xaml))
 * [MainWindow.xaml.cs](./CS/WpfPivotGridFilterValues/MainWindow.xaml.cs) (VB: [MainWindow.xaml.vb](./VB/WpfPivotGridFilterValues/MainWindow.xaml.vb))
 <!-- default file list end -->
-# How to: Apply Filter to a Field
+# How to: Apply a Filter to a Field
 
+This example shows how to apply a filter to a field. 
 
-<p>This example shows how to apply a filter to a field. For this, a new FilterFieldValues method was created. <br>The first condition will select records which contain 'UK' in the 'fieldCountry' field. The second condition will filter the 'Category' field to display only four categories: 'Beverages', 'Condiments', 'Seafood', and 'Produce'.</p>
+The first filter select records which contain _UK_ in the '**fieldCountry**' field, the second filter selects four categories in the **Category Name** field: _Beverages_, _Condiments_, _Seafood_, and _Produce_.
 
-<br/>
-
-
+![screenshot](images/screenshot.png)
--- VB/WpfPivotGridFilterValues/MainWindow.xaml.vb
+++ VB/WpfPivotGridFilterValues/MainWindow.xaml.vb
@@ -1,48 +1,17 @@
-﻿Imports System.Windows
-Imports System
-Imports DevExpress.Xpf.PivotGrid
-Imports System.Collections.Generic
+﻿Imports DevExpress.Xpf.PivotGrid
 
 Namespace WpfPivotGridFilterValues
-    ''' <summary>
-    ''' Interaction logic for MainWindow.xaml
-    ''' </summary>
-    Partial Public Class MainWindow
-        Inherits Window
+	''' <summary>
+	''' Interaction logic for MainWindow.xaml
+	''' </summary>
+	Partial Public Class MainWindow
+		Inherits DevExpress.Xpf.Core.ThemedWindow
 
-        Public Sub New()
-            InitializeComponent()
+		Public Sub New()
+			InitializeComponent()
 
-            ' Filters values in the Country field using the created method.
-            FilterFieldValues(fieldCountry,
-                  New String() {"UK"}, FieldFilterType.Included)
-
-            ' Filters values in the Category field using the created method.
-            FilterFieldValues(fieldCategory,
-                  New String() {"Beverages", "Condiments", "Seafood", "Produce"}, FieldFilterType.Included)
-        End Sub
-
-        ' Creates a new method that allows filtering field values.
-        Private Function FilterFieldValues(ByVal field As PivotGridField,
-                                           ByVal filterValues() As Object,
-                                           ByVal filterType As FieldFilterType) As Object()
-            ' Locks the control to prevent its excessive updates when multiple properties are modified.
-            pivotGridControl1.BeginUpdate()
-            Try
-                ' Clears the filter value collection and adds object items to it.
-                field.FilterValues.Clear()
-                For Each filterValue As Object In filterValues
-                    field.FilterValues.Add(filterValue)
-                Next filterValue
-            Finally
-                ' Specifies that the control should only display records 
-                ' which contain the specified values in the field.
-                field.FilterValues.FilterType = filterType
-
-                ' Unlocks the control.
-                pivotGridControl1.EndUpdate()
-            End Try
-            Return filterValues
-        End Function
-    End Class
+			fieldCountry.SetFilterValues(New String() { "UK" }, FieldFilterType.Included, True)
+			fieldCategoryName.SetFilterValues(New String() { "Beverages", "Condiments", "Seafood", "Produce" }, FieldFilterType.Included, True)
+		End Sub
+	End Class
 End Namespace
